609e84dc4da805ff8da5c97632549d5e43a85fd0
Andrew Pendleton <andrew.d.pendleton@gmail.com>
2016-01-12 19:11:18 -0600

    UPDATE comments
    Updated comments outlining the program

--- Program.cs
+++ Program.cs
@@ -13,11 +13,20 @@ namespace Hangman
             
             // Pick a Word
             
-            // Draw the Scene
+            // LOOP
+                // Draw the Scene
+                
+                // Ask the person to guess a letter and store it
+                
+                // If that letter is in the word, show that letter - then ask again?
+                
+                // Otherwise that letter isn't in the word, we add a piece to hangman
+                
+                // Check if they win
+                // They win IF the hidden word is equal to the guessed word, end the game
+                // They lost IF they have 5 bad guesses
             
-            // Ask the person to guess a letter
-            
-            // If that letter is in the word, show that letter
+            // Go back up to "Draww the Scene"
         }
     }
 }
