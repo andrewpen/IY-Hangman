9c7985a6f552874071ec2349de9c40739a6d4dec
Andrew Pendleton <andrew.d.pendleton@gmail.com>
2016-01-12 19:02:58 -0600

    ADD blueprint comments

--- Program.cs
+++ Program.cs
@@ -9,25 +9,15 @@ namespace Hangman
     {
         public static void Main(string[] args)
         {
-            //GO HERE
-            Console.WriteLine("Hello World");
-            string input = Console.ReadLine();
+            // Introduction Statement
             
-            for (int i = 0; i < 10; (i)++)
-            {
-                Console.WriteLine("The number is: " + i);
-            }
+            // Pick a Word
             
-            if (input == "Hi")
-            {
-                // Do this if true    
-                Console.WriteLine("Hello Back to you!");
-            }
-            else 
-            {
-                // Do this if false
-                Console.WriteLine("You did not say Hi!");
-            }
+            // Draw the Scene
+            
+            // Ask the person to guess a letter
+            
+            // If that letter is in the word, show that letter
         }
     }
 }
